df254238750208520ccec80b6314b114a2241f29
PhucLH <longdh@twinger.vn>
2024-09-26 16:03:18 +0700

    add clickout side modal

--- src/small-components/Modal/index.jsx
+++ src/small-components/Modal/index.jsx
@@ -5,9 +5,14 @@ const Modal = ({ isOpen, onClose, children }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="absolute inset-0 bg-gray-300 bg-opacity-50  h-full  flex items-center justify-center z-50 overflow-y-auto">
+        <div
+            onClick={(e) => {
+                if (e.target.id === "modal") onClose();
+            }}
+            id="modal"
+            className="absolute inset-0 bg-gray-300 bg-opacity-50  h-full  flex items-center justify-center z-50 overflow-y-auto"
+        >
             <div className="bg-white w-auto mx-auto p-6 rounded-lg shadow-lg relative mt-52  md:mt-0">
-
                 {/* Modal Content */}
                 <div>{children}</div>
 
@@ -15,12 +20,11 @@ const Modal = ({ isOpen, onClose, children }) => {
                 <Button
                     text={`Đóng`}
                     onClick={onClose}
-                    type={'danger'}
+                    type={"danger"}
                     className="mt-5"
                 />
             </div>
         </div>
-
     );
 };
 
